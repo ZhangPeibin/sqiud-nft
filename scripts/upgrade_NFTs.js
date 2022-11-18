5ac885d4b5c2f597a0652f472b8007cf7dac1eea
ivicmad <victor.l@lineir.me>
2022-11-18 17:15:25 +0200

    BSW-2250:ADD: Change NFTs contracts for collectiblesNFT

--- scripts/upgrade_NFTs.js
+++ scripts/upgrade_NFTs.js
@@ -4,24 +4,31 @@ const playerNFTAddress = `0xb00ED7E3671Af2675c551a1C26Ffdcc5b425359b`;
 const busNFTAddress = `0x6d57712416eD4890e114A37E2D84AB8f9CEe4752`;
 const ownerAddress = `0xbafefe87d57d4c5187ed9bd5fab496b38abdd5ff`;
 
+async function impersonateAccount(acctAddress) {
+    await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [acctAddress],
+    });
+    return await ethers.getSigner(acctAddress);
+}
 
 async function main() {
-    let accounts = await ethers.getSigners();
-    console.log(`Deployer address: ${ accounts[0].address}`);
-    if(accounts[0].address.toLowerCase() !== ownerAddress.toLowerCase()){
-        console.log(`Change deployer address. Current deployer: ${accounts[0].address}. Owner: ${ownerAddress}`);
+    let deployer = network.name === `localhost` ? await impersonateAccount(ownerAddress) : (await ethers.getSigners())[0];
+    if(deployer.address.toLowerCase() !== ownerAddress.toLowerCase()){
+        console.log(`Change deployer address. Current deployer: ${deployer.address}. Owner: ${ownerAddress}`);
         return;
     }
-    let nonce = await network.provider.send(`eth_getTransactionCount`, [accounts[0].address, "latest"]) - 1;
 
     console.log(`Start upgrade Bus NFT contract`);
-    const BusNFT = await ethers.getContractFactory(`SquidBusNFT`);
+    const BusNFT = await ethers.getContractFactory(`SquidBusNFT`, deployer);
+    // await upgrades.forceImport(busNFTAddress, BusNFT);
     const busNft = await upgrades.upgradeProxy(busNFTAddress, BusNFT);
     await busNft.deployed();
     console.log(`Bus NFT upgraded`);
 
-    console.log(`Start upgrade Bus NFT contract`);
-    const PlayerNFT = await ethers.getContractFactory(`SquidBusNFT`);
+    console.log(`Start upgrade SquidPlayer NFT contract`);
+    const PlayerNFT = await ethers.getContractFactory(`SquidPlayerNFT`, deployer);
+    // await upgrades.forceImport(playerNFTAddress, PlayerNFT);
     const playerNft = await upgrades.upgradeProxy(playerNFTAddress, PlayerNFT);
     await playerNft.deployed();
     console.log(`Player NFT upgraded`);
